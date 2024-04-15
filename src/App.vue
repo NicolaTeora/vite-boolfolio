933f67f675c70815d120e72a21e483f808348450
NicolaTeora <nikxodia@gmail.com>
2024-04-15 19:52:07 +0200

    feat : debug error

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,15 @@ import axios from 'axios';
 export default {
   data(){
     return{
-      test: 'hello'
+      test: 'hello',
+      projects: []
     }
   },
 
   created(){
     axios.get('http://127.0.0.1:8000/api/projects').then((response)=>{
-
+      this.projects = response.data.data;
+      console.log(response.data.data)
     });
   }
 }
@@ -21,6 +23,18 @@ export default {
 
     <div class="container">
       <h1>{{ test }}</h1>
+      <div class="row g-2">
+        <div v-for="project in projects" class="col-4">
+        <div class="card h-100">
+          <div class="card-body">
+            <img :src="project.image" alt="" class="card-img-top">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <p class="card-text">{{ project.description }}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        </div>
+      </div>
     </div>
   
   </div>

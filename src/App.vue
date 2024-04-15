2236bff3366d94dcb2993638d8450e4027f81288
NicolaTeora <nikxodia@gmail.com>
2024-04-15 18:58:40 +0200

    feat : start project ; organize scaffolding;

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data(){
+    return{
+      test: 'hello'
+    }
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+
+    <div class="container">
+      <h1>{{ test }}</h1>
+    </div>
+  
+  </div>
+</template>
+
+<style lang="scss">
+@use './src/scss/general.scss'
+</style>
